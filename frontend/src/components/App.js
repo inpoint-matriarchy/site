bc5c45b03c7f2622ffe456be8f61ceab4f685033
Ivan <vbcfgfgf@mail.ru>
2020-11-30 20:49:55 +0300

    add frontend v0.1

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import Maini from './Main';
 
 
 class App extends Component {
     render() {
-        return <div>
-            <h2>a</h2>
-            <h3>aга</h3>
-        </div>
+        return <Maini/> 
     }
 }
 
