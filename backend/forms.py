c87a372559c527805cf0168062bc4dc5ee99596e
Pavel <pavivin@yandex.ru>
2020-11-30 20:55:00 +0300

    change model

---
+++ backend/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class LoginForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.passwordInput)
+
+    def __init__(self):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Логин'
+        self.fields['password'].label = 'Пароль'
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+
+        if not User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'Пользователь {username} не найден')
+        user = User.objects.filter(username=username).first()
+        if user:
+            if not user.check_password(password):
+                raise forms.ValidationError('Неверный пароль')
+
+        return self.cleaned_data
