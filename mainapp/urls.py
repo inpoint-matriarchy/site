b82f74bd361b88d4bbdc5a506e6df9d68a45c8fe
Pavel <pavivin@yandex.ru>
2020-12-02 00:00:58 +0300

    add api

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
+import backend.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
-    path('api/', include('backend.urls')),
+    # path('course/', include('backend.urls')),
+    path('api/', backend.views.get_list),
 ]
